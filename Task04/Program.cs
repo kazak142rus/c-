c86c6819e12b1692e3f077876498da531ff9153e
Grogo <n89000547345@yandex.ru>
2023-12-18 01:28:20 +0700

    cod for task04

---
+++ Task04/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+Console.WriteLine("Введите число 1");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 3");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if (max<a){
+ max = a;
+} if (max<b){
+    max = b;
+} if (max<c){
+    max = c;
+
+}
+    Console.WriteLine($"Максимальное из введенных чисел : {max}");
+
+
