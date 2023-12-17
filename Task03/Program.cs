85e358e4170dcb01e26b5bb2b5ef6a24bdd58e45
Grogo <n89000547345@yandex.ru>
2023-12-17 17:43:30 +0700

    second way

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -4,27 +4,46 @@
 // 5 -> Пятница
 
 Console.WriteLine("Hello, enter number from 1 to 7");
-int number = Convert.ToInt32(Console.ReadLine());
-if (number == 1){
-    Console.WriteLine("Monday");
-} else if (number == 2){
-    Console.WriteLine("Tuesday");
+string number = Console.ReadLine();
+switch(number){
+    case "1": Console.WriteLine("Monday");
+    break;
+    case "2":Console.WriteLine("Tuesday");
+    break;
+    case "3":Console.WriteLine("Wednesday");
+    break;
+    case  "4":Console.WriteLine("Thursday");
+    break;
+    case  "5":Console.WriteLine("Friday");
+    break;
+    case  "6":Console.WriteLine("Satarday");
+    break;
+    case  "7":Console.WriteLine("Sunday");
+    break;
+    default:Console.WriteLine("Wrong number.");
+    break;
 }
-else if (number == 3){
-    Console.WriteLine("Wednesday");
-}
-else if (number == 4){
-    Console.WriteLine("Thursday");
-}
-else if (number == 5){
-    Console.WriteLine("Friday");
-}
-else if (number == 6){
-    Console.WriteLine("Saturday");
-}
-if (number == 7){
-    Console.WriteLine("Sunday");
-}
-else {
-    Console.WriteLine("Wrong number.");
-}
+
+// if (number == 1){
+//     Console.WriteLine("Monday");
+// } else if (number == 2){
+//     Console.WriteLine("Tuesday");
+// }
+// else if (number == 3){
+//     Console.WriteLine("Wednesday");
+// }
+// else if (number == 4){
+//     Console.WriteLine("Thursday");
+// }
+// else if (number == 5){
+//     Console.WriteLine("Friday");
+// }
+// else if (number == 6){
+//     Console.WriteLine("Saturday");
+// }
+// if (number == 7){
+//     Console.WriteLine("Sunday");
+// }
+// else {
+//     Console.WriteLine("Wrong number.");
+// }
