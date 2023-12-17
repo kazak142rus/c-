8b58a310304c0deddb89fd6d52f285f66ad09d6c
Grogo <n89000547345@yandex.ru>
2023-12-17 23:29:36 +0700

    cod for task07

---
+++ Task07/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход 
+// трёхзначное число и на выходе показывает последнюю цифру 
+// этого числа.
+// 456 -> 6
+// 782 -> 2
+// 918 -> 8
+
+Console.WriteLine("Hello, enter a three-digit number");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number<0){
+    number = number* (-1);
+}
+if (number >= 100 && number <= 999)
+{
+int lastDigit = number %10;
+Console.WriteLine($"Last digit of three-digit numer: {lastDigit}");
+}
+else
+{
+    Console.WriteLine("Wrong number");
+}
