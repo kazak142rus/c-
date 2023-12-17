a40260de05f9490c55ee575ede4594e1bb1a4142
Grogo <n89000547345@yandex.ru>
2023-12-17 17:27:39 +0700

    cod for task03

---
+++ Task03/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая будет выдавать
+// название дня недели по заданному номеру.
+// 3 -> Среда 
+// 5 -> Пятница
+
+Console.WriteLine("Hello, enter number from 1 to 7");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number == 1){
+    Console.WriteLine("Monday");
+} else if (number == 2){
+    Console.WriteLine("Tuesday");
+}
+else if (number == 3){
+    Console.WriteLine("Wednesday");
+}
+else if (number == 4){
+    Console.WriteLine("Thursday");
+}
+else if (number == 5){
+    Console.WriteLine("Friday");
+}
+else if (number == 6){
+    Console.WriteLine("Saturday");
+}
+if (number == 7){
+    Console.WriteLine("Sunday");
+}
+else {
+    Console.WriteLine("Wrong number.");
+}
